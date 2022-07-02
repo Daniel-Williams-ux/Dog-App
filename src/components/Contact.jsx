dcb5b34cc81e6bef6967da1530a54717cf0abae9
Daniel Williams <williamsoligodaniel@gmail.com>
2022-07-02 11:54:00 +0000

    added props to img

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Contact(props) {
     return (
         <div className="contact-card">
-            <img src="./images/cena.png" width="300px"/>
+            <img src={props.img} width="300px"/>
             <h3>{props.name}</h3>
             <div className="info-group">
                 <img src="./images/phone-icon.png" width="20px" />
